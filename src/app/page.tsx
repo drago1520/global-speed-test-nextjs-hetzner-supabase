8470f05b8ba75a464e9b9aa64ea1318ed4766eeb
dragomir1520 <dragomir1520@gmail.com>
2025-07-31 16:43:38 +0300

    db latency benchmark

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import { db } from "@/lib/drizzle";
 
 export default async function Page() {
+  const startTime = performance.now();
   const todos = await db.query.todo.findMany()
+  const endTime = performance.now();
+  const latencyMs = endTime - startTime;
   return (
     <pre>
       {new Date().toLocaleTimeString()}
+      DB Query latency: {latencyMs.toFixed(2)}
       {JSON.stringify(todos)}
     </pre>
   );
