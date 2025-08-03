53dd1cdc4e71360fca12ebb266eb1ab19c2d8d44
dragomir1520 <dragomir1520@gmail.com>
2025-08-03 21:53:57 +0300

    1 page with: - neon http - neon pg - local pg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,28 @@
-import { db } from "@/lib/drizzle";
+import { db, db2, db3 } from "@/lib/drizzle";
 
 export default async function Page() {
-  const startTime = performance.now();
-  const todos = await db.query.todo.findMany()
-  const endTime = performance.now();
-  const latencyMs = endTime - startTime;
+  const localStart = performance.now();
+  const _todos = await db.query.todo.findMany()
+  const localEnd = performance.now();
+  const localLatency = localEnd - localStart;
+
+  const httpStart = performance.now()
+  const _todosHTTP = await db3.query.todo.findMany()
+  const httpEnd = performance.now()
+  const httpLatency = httpEnd - httpStart
+
+  const neonPgStart = performance.now()
+  const _neonPgTodos = await db2.query.todo.findMany()
+  const neonPgEnd = performance.now()
+  const neonPgLatency = neonPgEnd - neonPgStart
+
   return (
     <>
       <h1>This is Coolify DB</h1>
-      <button><a href="/neon-no">Neon No replicas test node-postgres</a></button>
-      <pre>
         {new Date().toLocaleTimeString()}
-        DB Query latency: {latencyMs.toFixed(2)}
-        {JSON.stringify(todos[0])}
-      </pre>
+        <h3>Neon HTTP DB Query latency: <span className="text-red-400">{httpLatency.toFixed(2)} ms</span></h3>
+        <h3>Local db with node-postgres DB Query latency: <span className="text-red-400">{localLatency.toFixed(2)}</span></h3>
+        <h3>Neon node-postgres (TCP) DB Query latency: <span className="text-red-400">{neonPgLatency.toFixed(2)} ms</span></h3>
     </>
   );
 }
--- src/lib/drizzle.ts
+++ src/lib/drizzle.ts
@@ -1,6 +1,8 @@
 // Make sure to install the 'pg' package 
 import { drizzle } from 'drizzle-orm/node-postgres';
+import { drizzle as httpDrizzle } from 'drizzle-orm/neon-http';
 import * as schema from '@/models/schema'
 
 export const db = drizzle(process.env.DATABASE_URL!, {schema});
 export const db2 = drizzle(process.env.NEON_URL!, {schema}); // neon-no-replicas
+export const db3 = httpDrizzle(process.env.NEON_URL!, {schema});
